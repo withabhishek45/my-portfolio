408043deb0468e953b14d073b943e3242a730c91
withabhishek45 <131639376+withabhishek45@users.noreply.github.com>
2024-02-20 18:17:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function openModal(image) {
+    document.getElementById('modal-img').src = image;
+    document.getElementById('modal').style.display = 'block';
+}
+
+function closeModal() {
+    document.getElementById('modal').style.display = 'none';
+}
+<script src="https://kit.fontawesome.com/a076d05399.js"></script>
